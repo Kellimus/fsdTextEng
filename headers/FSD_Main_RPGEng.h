59b46153295f5164d7f36c3b92e45cfbd562e0ef
Kelly Christus <kellimus@gmail.com>
2021-09-22 14:39:22 -0600

    Add files via upload

---
+++ headers/FSD_Main_RPGEng.h
@@ -0,0 +1,46 @@
+/*
+*******************************************************************************
+*******************************************************************************
+*******************************************************************************
+*
+*	Project: Quest For The Sword
+*	File: FSD_Main_RPGEng.h
+*	Author: Kelly Christensen
+*	File Version: 1.00
+*	Engine Version: 0a.2.1.1r (work out versioning system later)
+*
+*	Description:
+*	This is the main Header File for the "Firestorm Productions Text RPG Engine".
+*	This file contains all of the programming and functionality needed to make a
+*	basic Text RPG through simple calls to the Engine.
+*
+*	Created on: September 7, 2016 6:54 PM
+*	Last updated on: September 22, 2021 2:22 PM
+* 
+* 
+*******************************************************************************
+*******************************************************************************
+*******************************************************************************
+*/
+#ifndef FSD_MAIN_RPGENG_H
+#define FSD_MAIN_RPGENG_H
+
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <stdlib.h>
+#include <unistd.h>
+//#include <vector>
+//#include <regex>
+#include "includes/libcolor.h"
+
+namespace FSD_RPG_ENG
+{
+	#include "includes/FSD_RPGEng_Classes.h"
+	#include "includes/FSD_RPGEng_Initialization.h"
+	#include "includes/FSD_RPGEng_Files.h"
+	#include "includes/FSD_RPGEng_GameFuncts.h"
+	#include "includes/FSD-RPGEng_Print.h"
+	#include "includes/FSD_RPGEng_PlayerFuncts.h"
+}
+#endif
